64026d40b619488ed3c27f02d6a8abcd04dede27
hellotheren <kirilldk20@gmail.com>
2026-07-25 02:30:33 +0300

    Update AnimationLogger.lua

--- AnimationLogger.lua
+++ AnimationLogger.lua
@@ -704,7 +704,7 @@ end
 
 task.spawn(function()
 	while not Closed do
-		task.wait(0)
+		task.wait(0.5)
 		local plr = game.Players.LocalPlayer
 		if not plr then break end
 		local char = plr.Character
@@ -715,7 +715,7 @@ task.spawn(function()
 		local obj = animator or hum
 		if not obj then continue end
 		for _, track in pairs(obj:GetPlayingAnimationTracks() or {}) do
-			task.wait(1)
+			task.wait(0.5)
 			if not Closed and AnimationLogger then
 				LogAnimation(track.Animation.Name, {
                     Path = track.Animation:GetFullName(),
